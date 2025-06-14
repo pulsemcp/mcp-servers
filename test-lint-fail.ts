94148982f95337bbd57e9ad892096674bd7d90b2
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2025-06-13 21:09:20 -0700

    test: remove lint error file (preparing for test case 2)

--- test-lint-fail.ts
+++
@@ -1,9 +0,0 @@
-// This file has intentional lint errors for testing
-const unusedVariable = "This variable is never used";
-
-function testFunction() {
-  console.log("Missing explicit return type")
-}
-
-// Inconsistent spacing and no semicolon
-const     badSpacing="value"
